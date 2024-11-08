6cc68614ffe633c6c392ad9aa228f9b9627f3c81
Mico <mico.masangkay02@gmail.com>
2024-11-07 21:25:16 -0500

    chore: initial project setup with FastAPI and database configuration

--- api/dependencies/database.py
+++ api/dependencies/database.py
@@ -1,20 +1,23 @@
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import conf
 from urllib.parse import quote_plus
 
-
-
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{conf.user}:{quote_plus(conf.password)}@{conf.host}:{conf.port}/{conf.database}?charset=utf8mb4"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+# Format the URL for MySQL connection
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{conf.user}:{quote_plus(conf.password)}@{conf.host}:{conf.port}/{conf.database}?charset=utf8mb4"
 )
+
+# Create an engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+# SessionLocal will create new sessions that we can use to interact with the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for our models to inherit from
 Base = declarative_base()
 
-
+# Dependency to get a database session for each request
 def get_db():
     db = SessionLocal()
     try:
